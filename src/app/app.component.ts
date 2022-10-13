f57dc433e725872c60cc5cf726d990383248d217
DT0998 <duongkytrieuvn@gmail.com>
2022-10-13 23:00:54 +0700

    feat: add smth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,18 @@ export class AppComponent {
     { type: "server", name: "Testserver", content: "Just a test!" },
     { type: "server", name: "Testserver", content: "Just a test!" },
   ];
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: "server",
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) { 
+    this.serverElements.push({
+      type: "server",
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent,
+    });
+  }
 }
