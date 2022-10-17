59db1ffc99721edfe48d99517a9d552c87e474f3
DT0998 <duongkytrieuvn@gmail.com>
2022-10-17 23:35:24 +0700

    feat: add smth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,18 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  serverElements = [
-    { type: "server", name: "Testserver", content: "Just a test!" },
-    { type: "server", name: "Testserver", content: "Just a test!" },
-  ];
-  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+  serverElements = [];
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
     this.serverElements.push({
       type: "server",
       name: serverData.serverName,
       content: serverData.serverContent,
     });
   }
-  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) { 
+  onBlueprintAdded(blueprintData: {
+    serverName: string;
+    serverContent: string;
+  }) {
     this.serverElements.push({
       type: "server",
       name: blueprintData.serverName,
